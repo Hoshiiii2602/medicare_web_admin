26caec553d9e29428824defd4862bce49c19414e
tuan.na <tuan.na@tinasoft.com.vn>
2025-08-05 21:14:50 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -44,6 +44,25 @@ export default defineConfig({
     "process.env": process.env, // Ensure this is included
   },
   base: "/admin",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
